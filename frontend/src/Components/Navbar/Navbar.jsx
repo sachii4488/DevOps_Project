fa317978e20780d6043ad08ec0bb3e066f7cc2c2
D.M.S.P. Disanayaka <151613583+sachii4488@users.noreply.github.com>
2025-02-26 15:32:01 +0530

    Test jenkins webhook

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         <div className="navbar">
             <div className="nav-logo">
                 <img src={logo} alt="logo" />
-                <p>Fur Friend Finder</p>
+                <p>Fur Friend Finder V2</p>
                 </div>
                 <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt=""/>
                 <ul ref={menuRef} className="nav-menu">
